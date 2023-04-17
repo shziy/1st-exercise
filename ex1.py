8d11cef348c2e04f6d55e7d1539eef5bec8c4dbc
shahrzad <129215346+shziy@users.noreply.github.com>
2023-04-17 21:13:18 +0330

    Update ex1.py
     editss va ezafe method va class

--- ex1.py
+++ ex1.py
@@ -55,3 +55,67 @@ class threedimensionals(shapes):
         
     def sides(self):
         pass
+        
+class cube(threedimensionals):
+   
+    def __init__(self,length,width,height):
+        
+        self.cube_sides=12
+        self.__shape_name='cube'
+        self.__having_volume='yes'
+        self.length=length
+        self.equal_sides='equal'
+        self.width=width
+        self.height=height
+    
+    def volume(self):
+        self.volume=self.length*self.height*self.width
+        return('the volume of cube is {}'.format(self.volume))
+        
+    def total_area(self):
+        self.total_area_cube=self.length**2*6
+        return('the total area of the cube is {} '.format(self.total_area_cube))
+    
+    def area(self):
+        return('it just has total area not area')
+    
+    def get_dimensions(self):
+        
+        super().dimensions()
+        
+    def shape_kind(self):
+        self.cube_shape='square'
+        return('the shape is square')
+        
+    def get__sides(self):
+        
+        return("the cube has 12 sides")
+        
+    def get_equal_sides(self):
+       
+        return('so all sides are {} '.format(self.equal_sides))
+#inja baraye inke  mokaab mostatil az khanevade mokaab hastaz cube ers bari karde ast      
+class rectangular_cube(cube):
+    def __init__(self,length,width,height):
+        super().__init__(length,width,height)
+        self.rec_cube_sides=self.cube_sides
+    #composiosion                      
+    def volume(self):
+        rectangularcube=cube(self.length,self.width,self.height)
+        return('the volume of the rectangular cube is {} '.format(rectangularcube.volume()))
+        
+    def total_area(self):
+        self.total_area=2*((self.width*self.height)+(self.width*self.length)+self.length*self.height)
+        return ('the totall area of the rectangular cube is {} '.format(self.total_area))        
+        
+    def shape_kind(self):
+        self.rectangular_cube_shape_kind='square and rectangle'
+        return ('the shap of the rectangular cube is {} '.format(self.rectangular_cube_shape_kind))
+         
+    def get__sides(self):
+
+        return('rectangular cube has 12 sides ')
+        
+    def get_dimensions(self):
+        super().dimensions()
+        
